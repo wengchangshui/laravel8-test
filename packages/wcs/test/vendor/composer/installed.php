a7d20e2d85f7401b706339d9b8ab2618fb98c970
wengchangshui <723944127@qq.com>
2021-10-09 16:33:31 +0800

    '--'

---
+++ packages/wcs/test/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ebecc2ce0e59341b41f1786a704d5066d0350ae7',
+    'name' => 'packages/package-test',
+  ),
+  'versions' => 
+  array (
+    'packages/package-test' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ebecc2ce0e59341b41f1786a704d5066d0350ae7',
+    ),
+  ),
+);
